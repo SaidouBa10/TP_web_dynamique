68844dcc25c7e3719128856a9ea0c0dd928979ce
SaidouBa10 <saidouba964@gmail.com>
2026-05-07 07:25:05 +0000

    Fix session stateless dashboard

--- src/main/java/com/example/tp1/security/SecurityConfig.java
+++ src/main/java/com/example/tp1/security/SecurityConfig.java
@@ -6,7 +6,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -28,17 +27,13 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable)
-                .sessionManagement(session -> session
-                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                )
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/auth/**").permitAll()
                         .requestMatchers("/h2-console/**").permitAll()
-                        .requestMatchers("/products", "/articles").permitAll()
-                        .requestMatchers("/dashboard").permitAll()  //
+                        .requestMatchers("/products/**", "/articles/**").permitAll()
+                        .requestMatchers("/dashboard").permitAll()
                         .anyRequest().authenticated()
                 )
-
                 .headers(headers -> headers
                         .frameOptions(frame -> frame.disable())
                 )
