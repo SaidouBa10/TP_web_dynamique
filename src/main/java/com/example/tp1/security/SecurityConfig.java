ac68b0861f586a031c03716f63bbb8afe838613a
SaidouBa10 <saidouba964@gmail.com>
2026-05-07 06:53:06 +0000

    Login Register Dashboard Thymeleaf

--- src/main/java/com/example/tp1/security/SecurityConfig.java
+++ src/main/java/com/example/tp1/security/SecurityConfig.java
@@ -33,7 +33,7 @@ public class SecurityConfig {
                 )
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/auth/**").permitAll()
-                        .requestMatchers("/h2-console/**").permitAll()
+                        .requestMatchers("/dashboard").permitAll()
                         .requestMatchers("/products", "/articles").permitAll()
                         .anyRequest().authenticated()
                 )
