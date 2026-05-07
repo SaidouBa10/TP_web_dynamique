a2df9304f287273c3af851fa4acbc43a8c525039
SaidouBa10 <saidouba964@gmail.com>
2026-05-07 07:13:34 +0000

    Fix dashboard access

--- src/main/java/com/example/tp1/security/SecurityConfig.java
+++ src/main/java/com/example/tp1/security/SecurityConfig.java
@@ -35,7 +35,7 @@ public class SecurityConfig {
                         .requestMatchers("/auth/**").permitAll()
                         .requestMatchers("/h2-console/**").permitAll()
                         .requestMatchers("/products", "/articles").permitAll()
-                        .requestMatchers("/dashboard").permitAll()  // ← ajoute cette ligne !
+                        .requestMatchers("/dashboard").permitAll()  //
                         .anyRequest().authenticated()
                 )
 
